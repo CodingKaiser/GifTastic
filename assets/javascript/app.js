1b29c1a114370ac902ebe4d882bbef1122bb3784
CodingKaiser <falkonoe@live.com>
2017-03-08 17:25:15 -0800

    Restructered code to be more object-oriented.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,61 +1,64 @@
 $(document).ready(function() {
-	var buttonLabels = ['game of thrones', 'hacker', 'mark zuckerberg', 'donald trump', 'horsey', 'dumb lion', 'dumb dog'];
-	var allowedRatings = ['g', 'pg'];
 
-	function start() {
-		for (var i = 0; i < buttonLabels.length; i++) {
-			makeNewButton(buttonLabels[i], true);
-		};
-	};
+	var gifApp = {
+		buttonLabels: ['game of thrones', 'hacker', 'mark zuckerberg', 'donald trump', 'horsey', 'dumb lion', 'dumb dog'],
+		allowedRatings: ['g', 'pg'],
 
-	function makeNewButton(term, isPrespecified) {
-		var newButton = $("<button></button>");
-		newButton.attr("data-search-term", term)
-		newButton.addClass("btn btn-default gif-button");
-		$("#button-container").append(newButton);
-		if (isPrespecified) {
-			// Simply display text
-			newButton.text(term);
-		} else {
-			// animate the appearance of text in button
-			newButton.typeIt({
-				strings: term,
-				lifeLike: false,
-				delay: 0,
-				speed: 10,
-				cursor: false,
-			});
-		}
-		newButton.on("click", getGifs);
-	};
+		start: function() {
+			for (var i = 0; i < this.buttonLabels.length; i++) {
+				this._makeNewButton(this.buttonLabels[i], true);
+			};
+		},
 
-	function getGifs() {
-		var searchTerm = $(this).attr("data-search-term");
-		$.ajax({
-			url: "http://api.giphy.com/v1/gifs/search?q=" + searchTerm +"&limit=30&rating&api_key=dc6zaTOxFJmzC",
-			method: "GET",
-		}).done(function(response) {
-			constructGifContainer(response);
-		});
-	};
-
-	function constructGifContainer(response) {
-		$("#gif-area").children().remove();
-		for (var i = 0; i < response.data.length; i++) {
-			if (allowedRatings.includes(response.data[i].rating)) {
-				var gifStill = response.data[i].images.downsized_still.url;
-				var gifMoving = response.data[i].images.downsized.url;
-				var gifRating = response.data[i].rating;
-				var newGifContainer = $("<div class='thumbnail gif-container'></div>");
-				newGifContainer.attr("data-gif-still", gifStill);
-				newGifContainer.attr("data-gif-moving", gifMoving);
-				newGifContainer.append("<div class='caption'><p class='gif-rating'>Rating: " + gifRating + "</p></div>");
-				newGifContainer.append("<img class='gif-img' src='" + gifStill + "'>");
-				newGifContainer.on("click", startPlayingGif);
-				$("#gif-area").append(newGifContainer);
+		_makeNewButton: function(term, isPrespecified) {
+			var newButton = $("<button></button>");
+			newButton.attr("data-search-term", term)
+			newButton.addClass("btn btn-default gif-button");
+			$("#button-container").append(newButton);
+			if (isPrespecified) {
+				// Simply display text
+				newButton.text(term);
+			} else {
+				// animate the appearance of text in button
+				newButton.typeIt({
+					strings: term,
+					lifeLike: false,
+					delay: 0,
+					speed: 10,
+					cursor: false,
+				});
 			}
-		}
-		console.log(response.data[0].images.downsized.url);
+			newButton.on("click", this._getGifs);
+		},
+
+		_getGifs: function() {
+			var searchTerm = $(this).attr("data-search-term");
+			$.ajax({
+				url: "http://api.giphy.com/v1/gifs/search?q=" + searchTerm +"&limit=30&rating&api_key=dc6zaTOxFJmzC",
+				method: "GET",
+			}).done(function(response) {
+				gifApp._constructGifContainer(response);
+			});
+		},
+
+		_constructGifContainer: function(response) {
+			$("#gif-area").children().remove();
+			for (var i = 0; i < response.data.length; i++) {
+				if (this.allowedRatings.includes(response.data[i].rating)) {
+					var gifStill = response.data[i].images.downsized_still.url;
+					var gifMoving = response.data[i].images.downsized.url;
+					var gifRating = response.data[i].rating;
+					var newGifContainer = $("<div class='thumbnail gif-container'></div>");
+					newGifContainer.attr("data-gif-still", gifStill);
+					newGifContainer.attr("data-gif-moving", gifMoving);
+					newGifContainer.append("<div class='caption'><p class='gif-rating'>Rating: " + gifRating + "</p></div>");
+					newGifContainer.append("<img class='gif-img' src='" + gifStill + "'>");
+					newGifContainer.on("click", startPlayingGif);
+					$("#gif-area").append(newGifContainer);
+				}
+			}
+			console.log(response.data[0].images.downsized.url);
+		},
 	};
 
 	function startPlayingGif() {
@@ -85,10 +88,10 @@ $(document).ready(function() {
 		if (inputBoxHasContent()) {
 			// make a new button and append it to existing buttons
 			var newSearchTerm = $("#search-term").val();
-			makeNewButton(newSearchTerm, false);
+			gifApp.makeNewButton(newSearchTerm, false);
 			clearInputBox();
 		}
 	});
 
-	start();
+	gifApp.start();
 });
